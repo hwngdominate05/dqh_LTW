2877387a21789913072eee119f1d82c663747849
hwngdominate05 <dhung2606@gmail.com>
2025-11-24 00:12:57 +0700

    Day12Lab:Lab1,2

---
+++ BTH_01/Models/Student.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BTH_02.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Email { get; set; }
+
+        public string? Password { get; set; }
+        public Branch? Branch { get; set; }
+        public Gender? Gender { get; set; }
+        public string? Address { get; set; }
+        public bool IsRegular { get; set; }
+        public DateTime DateOfBirth { get; set; }
+
+        [NotMapped] 
+        [Display(Name = "Upload Photo")]
+        public IFormFile Photo { get; set; }
+
+        public string? PhotoPath { get; set; }
+
+    }
+}
